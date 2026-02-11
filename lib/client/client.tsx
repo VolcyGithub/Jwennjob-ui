4cb4840e7ec112fc9558e4f3e7ba40c36d709e3a
Kingsley <kingsleyguillaume77@gmail.com>
2026-02-11 13:09:34 -0500

    Total restructuration of the folders

--- lib/client/client.tsx
+++ lib/client/client.tsx
@@ -1,37 +1,18 @@
 
 import { API_KEY, API_URL } from "@/config/constants";
 import { ApiClient } from "@/utils/class/client";
+import getCookies from "@/utils/functions/GetCookies";
 
-export const publicClient = new ApiClient({
-  baseURL: API_URL,
-});
+export const publicClient = new ApiClient({baseURL: API_URL});
 
-export const candidateClient = new ApiClient({
-  baseURL: API_URL,
-});
+export const candidateClient = new ApiClient({baseURL: API_URL});
 
-export const recruiterClient = new ApiClient({
-  baseURL: API_URL,
-});
+export const recruiterClient = new ApiClient({baseURL: API_URL});
 
-export const publicClientKey = new ApiClient({
-  baseURL: API_URL,
-});
+export const publicClientKey = new ApiClient({baseURL: API_URL});
 
-candidateClient.useBearerAuth(
-  () =>
-    document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("candidate_token="))
-      ?.split("=")[1],
-);
+candidateClient.useBearerAuth(() => getCookies('candidate_token'));
 
-recruiterClient.useBearerAuth(
-  () =>
-    document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("recruiter_token="))
-      ?.split("=")[1],
-);
+recruiterClient.useBearerAuth(() => getCookies('recruiter_token'));
 
 publicClientKey.useApiKeyAuth(() => API_KEY);
---
+++ utils/functions/GetCookies.tsx
@@ -0,0 +1,9 @@
+
+const getCookies = (name : string) => {
+     return document.cookie
+      .split("; ")
+      .find((row) => row.startsWith(name))
+      ?.split("=")[1]
+}
+
+export default getCookies;
